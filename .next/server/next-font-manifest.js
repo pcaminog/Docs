8f7e03cde577ffe69bf306566a369f5c7b872e9b
Pablo Camino <pablo@cloudflare.com>
2023-12-22 08:10:33 -0600

    c

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/pablo/projects/mon1tor-docs/src/app/layout\":[\"static/media/c9a5bc6a7c948fb0-s.p.woff2\",\"static/media/8935352d0bfcf3a9-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
