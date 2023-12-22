fffe4aa4dba042528169d0f84080ad739595406f
Pablo Camino <pablo@cloudflare.com>
2023-12-22 08:00:13 -0600

    c

--- src/lib/navigation.ts
+++ src/lib/navigation.ts
@@ -8,31 +8,31 @@ export const navigation = [
   {
     title: 'Monitors',
     links: [
-      { title: 'Standard HTTP', href: '/docs/understanding-caching' },
+      { title: 'Standard HTTP', href: '/docs/standard-http' },
       {
         title: 'Code HTTP',
-        href: '/docs/predicting-user-behavior',
+        href: '/docs/code-http',
       },
-      { title: 'DNS', href: '/docs/basics-of-time-travel' }
+      { title: 'DNS', href: '/docs/dns' }
       
     ],
   },
   {
     title: 'Alerts',
     links: [
-      { title: 'What is an Alert?', href: '/docs/writing-plugins' },
-      { title: 'Close', href: '/docs/neuralink-integration' },
-      { title: 'Acknowledge', href: '/docs/temporal-paradoxes' },
-      { title: 'Testing', href: '/docs/testing' },
+      { title: 'What is an Alert?', href: '/docs/what-is-an-alert' },
+      { title: 'Close alerts', href: '/docs/close-alerts' },
+      { title: 'Acknowledge alerts', href: '/docs/acknowledge-alerts' },
+      { title: 'Testing alerts', href: '/docs/testing-alerts' },
     ],
   },
   {
     title: 'Notifications',
     links: [
-      { title: 'Types', href: '/docs/cacheadvance-predict' },
-      { title: 'Email', href: '/docs/cacheadvance-flush' },
-      { title: 'Webhook', href: '/docs/cacheadvance-revert' },
-      { title: 'Integrations', href: '/docs/cacheadvance-regret' },
+      { title: 'Notification types', href: '/docs/notification-types' },
+      { title: 'Email notification', href: '/docs/email-notification' },
+      { title: 'Webhook notification', href: '/docs/webhook-notification' },
+      { title: 'Integrations notification', href: '/docs/integrations-notification' },
     ],
   },
   {
